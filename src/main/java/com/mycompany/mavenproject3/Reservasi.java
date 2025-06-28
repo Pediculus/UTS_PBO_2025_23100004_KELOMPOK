4f9c2b39ff59bc452d00f49828ee01e99b9efad9
Pediculus <162426933+Pediculus@users.noreply.github.com>
2025-06-28 08:16:18 +0700

    Update Reservasi.java

--- src/main/java/com/mycompany/mavenproject3/Reservasi.java
+++ src/main/java/com/mycompany/mavenproject3/Reservasi.java
@@ -1,34 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject3;
 
 import java.util.Date;
 import java.time.LocalTime;
+
 /**
  *
  * @author HAK_PHENG
  */
-
 public class Reservasi {
     private String kodeReservasi;
     private String customerName;
     private Date tanggalReservasi;
-    private String masaWaktuReservasi;
+    private LocalTime masaWaktuReservasi;
     private String detailReservasi;
-    
-    public Reservasi(String kodeReservasi, String customerName, Date tanggalReservasi, String masaWaktuReservasi, String detailReservasi) {
+
+    public Reservasi(String kodeReservasi, String customerName, Date tanggalReservasi, LocalTime masaWaktuReservasi, String detailReservasi) {
         this.kodeReservasi = kodeReservasi;
         this.customerName = customerName;
         this.tanggalReservasi = tanggalReservasi;
         this.masaWaktuReservasi = masaWaktuReservasi;
         this.detailReservasi = detailReservasi;
     }
-    
+
     public String getKodeReservasi() {
         return kodeReservasi;
     }
+
     public void setKodeReservasi(String kodeReservasi) {
         this.kodeReservasi = kodeReservasi;
     }
@@ -36,6 +33,7 @@ public class Reservasi {
     public String getCustomerName() {
         return customerName;
     }
+
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
@@ -43,20 +41,23 @@ public class Reservasi {
     public Date getTanggalReservasi() {
         return tanggalReservasi;
     }
+
     public void setTanggalReservasi(Date tanggalReservasi) {
         this.tanggalReservasi = tanggalReservasi;
     }
 
-    public String getMasaWaktuReservasi() {
+    public LocalTime getMasaWaktuReservasi() {
         return masaWaktuReservasi;
     }
-    public void setMasaWaktuReservasi(String masaWaktuReservasi) {
+
+    public void setMasaWaktuReservasi(LocalTime masaWaktuReservasi) {
         this.masaWaktuReservasi = masaWaktuReservasi;
     }
 
     public String getDetailReservasi() {
         return detailReservasi;
     }
+
     public void setDetailReservasi(String detailReservasi) {
         this.detailReservasi = detailReservasi;
     }
